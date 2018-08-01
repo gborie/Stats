61e73e298ed7955b436eb813560a80f91ce56f34
gborie <gborie7@gmail.com>
2018-08-01 17:00:39 +0100

    Updated libraries

--- esi_monthly.py
+++ esi_monthly.py
@@ -1,5 +1,8 @@
+# Commands to use in Jupyter Notebook
+
 # Libraries
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 from sklearn.linear_model import LinearRegression as LinReg
+%matplotlib inline
